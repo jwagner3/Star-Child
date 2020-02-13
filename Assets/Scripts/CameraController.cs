ec3b697c7521311907c87253c770383e3eb2a54c
Jackson Wagner <jwagner3@stedwards.edu>
2020-02-12 20:34:48 -0600

    First Push

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Vector3 worldpos;
+    private float mouseX;
+    private float mouseY;
+    private float cameraDif;
+
+    public Camera mainCamera;
+
+    public GameObject fpc;
+
+    void Start()
+    {
+        cameraDif = mainCamera.transform.position.y - fpc.transform.position.y;
+    }
+
+    void LookAtMouse()
+    {
+        mouseX = Input.mousePosition.x;
+
+        mouseY = Input.mousePosition.y;
+
+        worldpos = mainCamera.ScreenToWorldPoint(new Vector3(mouseX, mouseY, cameraDif));
+
+        Vector3 turretLookDirection = new Vector3(worldpos.x, fpc.transform.position.y, worldpos.z);
+
+        fpc.transform.LookAt(turretLookDirection);
+    }
+}
---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float rotateSpeed = 1;
+
+
+ 
+    private Vector3 moveDirection = Vector3.zero;
+    private Quaternion moveRotation;
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            transform.LookAt(hit.point);
+            //CameraRotate();
+            //StartCoroutine("cameraTimer");
+        }
+        //Feed moveDirection with input.
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Zorical"), Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+        
+        //Jumping
+        controller.Move(moveDirection * Time.deltaTime);
+
+    }
+
+
+
+    // IEnumerator cameraTimer()
+    //{
+    //    yield return new WaitForSeconds(.1f);
+    //    CameraRotate();
+    //}
+        
+    //private void CameraRotate()
+    //{
+    //    mainCamera.transform.rotation = gameObject.transform.rotation;
+    //}
+       
+    }
